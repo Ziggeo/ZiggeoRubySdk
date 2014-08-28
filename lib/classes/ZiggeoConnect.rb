b50cde72eb1b4326b16596434b1e65f69e62f281
Oliver Friedmann <oliver.friedmann@gmail.com>
2014-08-28 14:13:05 -0400

    SSL Fix

--- lib/classes/ZiggeoConnect.rb
+++ lib/classes/ZiggeoConnect.rb
@@ -2,11 +2,10 @@ require 'net/http'
 require 'json'
 
 class ZiggeoConnect
-  
   def initialize(application)
     @application = application
   end
-  
+
   def request(method, path, data = nil, file = nil)
     url = URI.parse(@application.config.server_api_url + '/v1' + path)
     if (method == "GET")
@@ -17,29 +16,34 @@ class ZiggeoConnect
     elsif (method == "POST")
       req = Net::HTTP::Post.new(url.to_s)
       if (data != nil)
-        req.req.set_form_data(data)
+      req.req.set_form_data(data)
       end
     elsif (method == "DELETE")
       req = Net::HTTP::Delete.new(url.to_s)
       if (data != nil)
-        req.req.set_form_data(data)
+      req.req.set_form_data(data)
       end
     end
     req.basic_auth(@application.token, @application.private_key)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
+    http = Net::HTTP.new(url.host, url.port)
+    if (url.scheme == "https")
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    res = http.start() {|agent|
+      agent.request(req)
     }
     return res.body
   end
-  
+
   def requestJSON(method, path, data = nil, file = nil)
     return JSON.parse self.request(method, path, data, file)
   end
-  
+
   def get(path, data = nil, file = nil)
     return self.request("GET", path, data, file)
   end
-  
+
   def getJSON(path, data = nil, file = nil)
     return self.requestJSON("GET", path, data, file)
   end
@@ -47,7 +51,7 @@ class ZiggeoConnect
   def post(path, data = nil, file = nil)
     return self.request("POST", path, data, file)
   end
-  
+
   def postJSON(path, data = nil, file = nil)
     return self.requestJSON("POST", path, data, file)
   end
@@ -55,7 +59,7 @@ class ZiggeoConnect
   def delete(path, data = nil, file = nil)
     return self.request("DELETE", path, data, file)
   end
-  
+
   def deleteJSON(path, data = nil, file = nil)
     return self.requestJSON("DELETE", path, data, file)
   end
