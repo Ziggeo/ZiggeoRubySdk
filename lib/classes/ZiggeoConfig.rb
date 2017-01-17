1eb05d51942c09ba817a919096ff3a926ca67397
Rashad Aliyev <aliyev.resad@gmail.com>
2017-01-18 01:48:05 +0400

    timeout options added also tested with fake server

--- lib/classes/ZiggeoConfig.rb
+++ lib/classes/ZiggeoConfig.rb
@@ -1,7 +1,9 @@
 class ZiggeoConfig
-  attr_accessor :server_api_url
+  attr_accessor :server_api_url, :request_timeout, :request_timeout_per_mb
 
   def initialize()
+    @request_timeout = 30 # seconds
+    @request_timeout_per_mb = 20 # seconds per MB of uploaded file
     @server_api_url = "https://srvapi.ziggeo.com"
   end
 end
