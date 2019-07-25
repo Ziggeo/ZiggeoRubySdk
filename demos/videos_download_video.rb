3cab8165d7e08a9abf6c6e3c627fdcb67469e15d
Natasha Rocha <nattycdb@gmail.com>
2019-07-25 11:19:12 -0300

    Create demos (videos)
    Created demo for all methods on ZiggeoVideos class, except analytics.

---
+++ demos/videos_download_video.rb
@@ -0,0 +1,35 @@
+=begin
+  This script shows you how to download a single video.
+
+  Parameters you need to pass:
+    1. APP_TOKEN
+    2. PRIVATE_KEY
+    3. VIDEO_TOKEN
+    4. FILE_NAME
+
+  Example call:
+    ruby videos_download_video.rb APP_TOKEN PRIVATE_KEY VIDEO_TOKEN
+=end
+
+require_relative "../lib/Ziggeo"
+
+app_token = ARGV[0]
+private_key = ARGV[1]
+video_token = ARGV[2]
+file_name = ARGV[3]
+
+@ziggeo = Ziggeo.new(app_token, private_key)
+
+# Get video extension and add it to filename
+video_data = @ziggeo.videos.get(video_token)
+file_extension = video_data["original_stream"]["video_type"]
+file_name = file_name + "." + file_extension
+
+# Download video
+video_blob = @ziggeo.videos.download_video(video_token)
+
+# Save video to file
+File.open(file_name, "w") {
+  |file|
+  file.print video_blob
+}
---
+++ demos/videos_push_to_service.rb
@@ -0,0 +1,26 @@
+=begin
+  This script explains how to push an existing video to an auto push service you have previously configured.
+
+  Parameters you need to pass:
+    1. APP_TOKEN
+    2. PRIVATE_KEY
+    3. VIDEO_TOKEN
+    4. PUSH_SERVICE_TOKEN
+
+  Example call:
+    ruby videos_count.rb APP_TOKEN PRIVATE_KEY VIDEO_TOKEN PUSH_SERVICE_TOKEN
+=end
+
+require_relative "../lib/Ziggeo"
+
+app_token = ARGV[0]
+private_key = ARGV[1]
+video_token = ARGV[2]
+push_service_token = ARGV[3]
+
+@ziggeo = Ziggeo.new(app_token, private_key)
+
+@ziggeo.videos.push_to_service(
+  video_token,
+  "pushservicetoken": push_service_token
+)
